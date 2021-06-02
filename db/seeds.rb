e6d19ca31a07a41bd933cc0e217f15a36de80452
landon hickman <landonhickman12@gmail.com>
2021-06-02 14:40:47 -0600

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Test.destroy_all
+
+Test.create(name: 'Test1')
+Test.create(name: 'Test2')
+Test.create(name: 'Test3')
+Test.create(name: 'Test4')
+Test.create(name: 'Test5')
+
+puts 'Seeded 5'
