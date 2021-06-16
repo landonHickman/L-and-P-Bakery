a65ac004b1c6f16fab1b5e2bc57c7a2e525ce8a8
landon hickman <landonhickman12@gmail.com>
2021-06-16 14:53:35 -0600

    created hook for form

---
+++ client/src/customHooks/useFormInput.js
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+
+export const useFormInput = (initialValue, label='') => {
+  const [formValue, setFormValue] =useState(initialValue)
+
+  //returning an object {} is necessary
+  return {
+    //label will go on top of input to describe what needs to be input
+    label,
+    //placeholder goes inside input fadded out to describe what needs to be input
+    placeholder: `Enter ${label}`,
+    //value is the default value of the input as well as what we will receive from the input.
+    value: formValue,
+    onChange: (e, {value})=>setFormValue(value)
+  }
+}
