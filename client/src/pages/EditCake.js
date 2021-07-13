6f5aeb5a7470c0a008537662bfd31b037dd2b52c
Atianna DeAngelo <atianna.deangelo@gmail.com>
2021-07-13 17:20:11 -0600

    changes to edit about

--- client/src/pages/EditCake.js
+++ client/src/pages/EditCake.js
@@ -1,65 +1,65 @@
-// import React from "react";
-// import { Form, Row, Col, Button } from "react-bootstrap";
-// import { styles } from "../styles/styles";
+import React from "react";
+import { Form, Row, Col, Button } from "react-bootstrap";
+import { styles } from "../styles/styles";
 
 
-// const EditCake = () => {
-//   return (
+const EditCake = () => {
+  return (
     
-//     <div>
-//         <Form>
-//         <Form.Group>
-//           <Row>
-//             <Col>
-//               <Form.Label>Cake Main Title</Form.Label>
-//               <Form.Control placeholder="Cake Main Title" />
-//               <Form.Label>Cake Description</Form.Label>
-//               <Form.Control placeholder="Cake Description" as="textarea" rows={2}/>
-//               <Form.Label>Cake Title</Form.Label>
-//               <Form.Control placeholder="Cake Title" />
-//               <Form.Label>Mousse Title</Form.Label>
-//               <Form.Control placeholder="Mousse Title" />
-//               <Form.Label>Triangle Title</Form.Label>
-//               <Form.Control placeholder="Triangle Title" />
-//               <Form.Label>Cake Size Title</Form.Label>
-//               <Form.Control placeholder="Size Title" />
-//               <Form.Label>Special order Title</Form.Label>
-//               <Form.Control placeholder="Special order Title" as="textarea" rows={4}/>
-//               <Form.Group controlId="formFile" className="mb-3">
-//               <Form.Label>Change Custom Cake Header</Form.Label>
-//               <Form.Control type="file" />
-//               <div style={styles.SmallPic}></div>
-//             </Form.Group>
-//             </Col>
-//             <Col>
-//             </Col>
-//           </Row>
-//         </Form.Group>
-//         <Button variant="primary" type="submit" block>
-//           Submit
-//         </Button>
-//         </Form>
-//         <Form style={{display: 'flex', flexDirection: 'column'}}>
-//         <Form.Group>
-//           <Row>
-//             <Col>
-//               <Form.Label>Cake Options to Action Button Text</Form.Label>
-//               <Form.Control placeholder="Cake Options to Action Button Text"/>
-//               <Form.Label>Mousse Options to Action Button Text</Form.Label>
-//               <Form.Control placeholder="Mousse Options to Action Title"/>
-//               <Form.Label>Triangle Options to Action Button Text</Form.Label>
-//               <Form.Control placeholder="Triangle Options to Action Title"/>
-//               <Form.Label>Cake Size Options to Action Button Text</Form.Label>
-//               <Form.Control placeholder="Cake Size Options to Action Title"/>
-//             </Col>
-//           </Row>
-//         </Form.Group>
-//         <Button variant="primary" type="submit" style={{textAlign: 'center'}}>
-//           Submit
-//         </Button>
-//         </Form>
-//     </div>
+    <div>
+        <Form>
+        <Form.Group>
+          <Row>
+            <Col>
+              <Form.Label>Cake Main Title</Form.Label>
+              <Form.Control placeholder="Cake Main Title" />
+              <Form.Label>Cake Description</Form.Label>
+              <Form.Control placeholder="Cake Description" as="textarea" rows={2}/>
+              <Form.Label>Cake Title</Form.Label>
+              <Form.Control placeholder="Cake Title" />
+              <Form.Label>Mousse Title</Form.Label>
+              <Form.Control placeholder="Mousse Title" />
+              <Form.Label>Triangle Title</Form.Label>
+              <Form.Control placeholder="Triangle Title" />
+              <Form.Label>Cake Size Title</Form.Label>
+              <Form.Control placeholder="Size Title" />
+              <Form.Label>Special order Title</Form.Label>
+              <Form.Control placeholder="Special order Title" as="textarea" rows={4}/>
+              <Form.Group controlId="formFile" className="mb-3">
+              <Form.Label>Change Custom Cake Header</Form.Label>
+              <Form.Control type="file" />
+              <div style={styles.SmallPic}></div>
+            </Form.Group>
+            </Col>
+            <Col>
+            </Col>
+          </Row>
+        </Form.Group>
+        <Button variant="primary" type="submit" block>
+          Submit
+        </Button>
+        </Form>
+        <Form style={{display: 'flex', flexDirection: 'column'}}>
+        <Form.Group>
+          <Row>
+            <Col>
+              <Form.Label>Cake Options Button Text</Form.Label>
+              <Form.Control placeholder="Cake Options to Title"/>
+              <Form.Label>Mousse Options Button Text</Form.Label>
+              <Form.Control placeholder="Mousse Options Title"/>
+              <Form.Label>Triangle Options Button Text</Form.Label>
+              <Form.Control placeholder="Triangle Options Title"/>
+              <Form.Label>Cake Size Options Button Text</Form.Label>
+              <Form.Control placeholder="Cake Size Options Title"/>
+            </Col>
+          </Row>
+        </Form.Group>
+        <Button variant="primary" type="submit" style={{textAlign: 'center'}}>
+          Submit
+        </Button>
+        </Form>
+    </div>
   
-//   );
-// };
-// export default EditCake;
+  );
+};
+export default EditCake;
