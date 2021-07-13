6179516dc95303c396c92b9ad9d46873bfa0a95e
landon hickman <landonhickman12@gmail.com>
2021-07-13 15:12:27 -0600

    grid 2 update complete

--- client/src/components/GridEditor2.js
+++ client/src/components/GridEditor2.js
@@ -1,21 +1,63 @@
-import React from 'react'
-import { Form, Button, Row, Col } from 'react-bootstrap'
-import { styles } from '../styles/styles'
+import axios from "axios";
+import React, { useState } from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { styles } from "../styles/styles";
 
-const GridEditor2 = () => {
+const GridEditor2 = (props) => {
+  const { landing } = props;
+  const [files, setFiles] = useState(
+    landing.main_background_img ? landing.main_background_img : ""
+  );
+  const [title, setTitle] = useState(
+    landing.grid_title_2 ? landing.grid_title_2 : ""
+  );
+  const [desc, setDesc] = useState(
+    landing.grid_description_2 ? landing.grid_description_2 : ""
+  );
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // if (files.length >= 1) {
+      //   let data = new FormData();
+      //   data.append("fileHere", files[0].file);
+      //   let res1 = await axios.post("/api/images/upload", data);
+      //   var img = res1.data.cloud_image.secure_url;
+      // }
+      let res = await axios.put(`/api/landing_pages/${landing.id}`, {
+        grid_title_2: title,
+        grid_description_2: desc,
+      });
+      console.log('update',res)
+    } catch (err) {
+      console.log("Inside handleSubmit catch", err);
+      console.log("Inside handleSubmit catch", err.response);
+    }
+  };
+
   return (
     <>
-      <Form>
-      <Row>
+      <Form onSubmit={handleSubmit}>
+        <Row>
           <Col>
             <div style={styles.SmallPic}></div>
           </Col>
           <Col>
             <Form.Group>
               <Form.Label>Grid Title 2</Form.Label>
-              <Form.Control placeholder="Grid Title 2" />
+              <Form.Control
+                placeholder="Grid Title 2"
+                defaultValue={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
               <Form.Label>Grid Description 2</Form.Label>
-              <Form.Control placeholder="Grid Description 2" />
+              <Form.Control
+                as="textarea"
+                rows={4}
+                placeholder="Grid Description 2"
+                defaultValue={desc}
+                onChange={(e) => setDesc(e.target.value)}
+              />
             </Form.Group>
             <Button variant="primary" type="submit" block>
               Submit
@@ -24,7 +66,7 @@ const GridEditor2 = () => {
         </Row>
       </Form>
     </>
-  )
-}
+  );
+};
 
-export default GridEditor2
+export default GridEditor2;
