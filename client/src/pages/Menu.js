b7a4d1ceece431c828a4f99e93bfb497bd6df331
Rodgerick Heninger <rodgerick.williams@icloud.com>
2021-07-14 14:33:36 -0600

    worked on fixing the menu page

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -1,89 +1,158 @@
-import React, { useState, useEffect } from "react";
-import CardGroup from "react-bootstrap/CardGroup";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+import React, { useState, useEffect, Component } from "react";
+import { Card, Spinner, Button } from "react-bootstrap";
 import Axios from "axios";
 import Footer from "../components/Footer"
 
-const Categories = () => {
-  const [categories, setCategories] = useState([]);
-  const [products, setProducts] = useState([]);
-  const [ products, setProduct] = useState([])
-  const [show, setShow] = useState(true);
+// const Menu = () => {
+//   //const [productId, categoryId] = props;
+//   const [categories, setCategories] = useState([]);
+//   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    getCategories();
-    getProducts();
-  }, []);
+//   useEffect(() =>{
+//     getCategories()
+//     //getProducts()
+//   }, [])
 
-  const getProduct = async () => {
-    let res = await axios.get(`/api/categories/${catId}/products/${productId}`);
-    // console.log("limited", res.data.limited_time);
-    // console.log("special", res.data.special_item_carousel);
-    setProduct(res.data);
-  };
+//   const getCategories = async () => {
+//     try {
+//       let res = await Axios.get(
+//         `/api/categories`
+//       );
+//       setCategories(res.data);
+//       console.log(res);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
 
-  const getCategories = () => {
-    Axios.get("/api/categories")
-      .then((res) => {
-        setCategories(res.data);
+// }
+// export default Menu;
+
+class Menu extends Component {
+  state = {
+    name: []
+  }
+  
+  componentDidMount() {
+    Axios.get(`/api/categories`)
+      .then(res => {
+        console.log(res)
+        this.setState({
+          name: res.data
+        })
       })
-      .catch((err) => {
-        alert("Error: couldnt find categories");
-      });
-  };
+  }
 
-  const getProducts = () => {
-    Axios.get("/api/products")
-      .then((res) => {
-        setProducts(res.data);
+  render() {
+    const { name } = this.state
+    const nameList = name.length ? (
+      name.map(name => {
+        return (
+          <div key={name.id}>
+            <div alignment='center'>
+              <Button variant="outline-primary">{name.name}</Button>
+            </div>
+          </div>
+        );
       })
-      .catch((err) => {
-        alert("Error: couldnt find products");
-      });
-  };
-
-  const findProductsByCategory = () => {
-    let productCategoryIds = [];
-    products.map((product) => {
-      productCategoryIds.push(product.category_id);
-    });
-    return productCategoryIds;
-  };
-
-  const renderCategories = () => {
-    let productCategoryIds = findProductsByCategory();
-    return categories.map((category) => {
-      let singleCategoryIdArray = productCategoryIds.filter(
-        (id) => id === category.id
-      );
-      return (
-        <div>
-
-          <Button variant="outline-primary" onClick={() => setShow(!show)}>Drinks</Button>{show ?
-            
-          <Card key={category.id}>
-            <Card.Body>
-              <Card.Title>{category.name}</Card.Title>
-              <Card.Text>{singleCategoryIdArray.length} Products</Card.Text>
-            </Card.Body>
-          </Card>
-            : null}
-    
-        </div>
-      );
-    });
-  };
-
-  return (
-    <>
-    <div className="Menu">
+    ) : (
+        <div className="center">Nothing to show</div>
+      )
+    return (
+      <div className="container">
+        <h4 className="center">Menu</h4>
+        {nameList}
+        <br/>
+        <Footer />
+      </div>
       
-      <Card>{renderCategories()}</Card> 
-    </div>
-    <Footer/>
-</>
-  );
-};
+    );
+  }
+}
+ 
+export default Menu;
 
-export default Categories;
+// import React, { useState, useEffect } from "react";
+// import CardGroup from "react-bootstrap/CardGroup";
+// import Card from "react-bootstrap/Card";
+// import Button from "react-bootstrap/Button";
+// import Axios from "axios";
+// import Footer from "../components/Footer";
+
+// const Categories = () => {
+//   const [categories, setCategories] = useState([]);
+//   const [products, setProducts] = useState([]);
+//   //const [products, setProduct] = useState([]);
+//   const [show, setShow] = useState(false);
+
+//   useEffect(() => {
+//     getCategories();
+//     getProducts();
+//   }, []);
+
+  
+
+//   const getCategories = () => {
+//     Axios.get(`/api/categories`)
+//       .then((res) => {
+//         setCategories(res.data);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   };
+
+//   const getProducts = () => {
+//     Axios.get("/api/products")
+//       .then((res) => {
+//         setProducts(res.data);
+//       })
+//       .catch((err) => {
+//         console.error(err)
+//       });
+//   };
+
+//   const findProductsByCategory = () => {
+//     let productCategoryIds = [];
+//     products.map((product) => {
+//       productCategoryIds.push(product.category_id);
+//     });
+//     return productCategoryIds;
+//   };
+
+//   const renderCategories = () => {
+//     let productCategoryIds = findProductsByCategory();
+//     return categories.map((category) => {
+//       let singleCategoryIdArray = productCategoryIds.filter(
+//         (id) => id === category.id
+//       );
+//       return (
+//         <div>
+//           <Button variant="outline-primary" onClick={() => setShow(!show)}>
+//             {category.name}
+//           </Button>
+//           {show ? (
+//             <Card key={category.id}>
+//               <Card.Body>
+//                 <Card.Title>{category.name}</Card.Title>
+//                 <Card.Text>{singleCategoryIdArray.length} Products</Card.Text>
+//               </Card.Body>
+//             </Card>
+//           ) : null}
+//         </div>
+//       );
+//     });
+//   };
+
+//   return (
+//     <>
+//       <div className="Menu">
+//         <Card>{renderCategories()}</Card>
+//       </div>
+//       <br/>
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default Categories;
