f7fddf97c0fbfc87e9ccd6a391a2cc51caa5d507
landon hickman <landonhickman12@gmail.com>
2021-07-22 10:41:27 -0600

    fixed product model to search by id

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,21 +7,24 @@ class Product < ApplicationRecord
   end
 
   def self.all_limited_time_cake_products
+    @category = Category.find_by(name: 'Cakes')
     select('products.id, name, image, price, description, limited_time, category_id')
     .where('limited_time = true')
-    .where('category_id = 39');
+    .where('category_id = ?', @category.id);
   end
 
   def self.all_limited_time_boba_products
+    @category = Category.find_by(name: 'Drinks')
     select('products.id, name, image, price, description, limited_time, category_id')
     .where('limited_time = true')
-    .where('category_id = 1');
+    .where('category_id = ?', @category.id);
   end
 
   def self.all_limited_time_bakery_products
+    @category = Category.find_by(name: 'Bakery')
     select('products.id, name, image, price, description, limited_time, category_id')
     .where('limited_time = true')
-    .where('category_id = 2');
+    .where('category_id = ?', @category.id);
   end
 
 end
